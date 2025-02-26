5d60ce6000b31cfbb72802c96624e928966cdc64
Ninaad Sawant <ninaadsawant23@gmail.com>
2025-02-26 17:22:30 -0500

    Login/Logout: BAckend

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from homepage import views
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("", views.home, name='homepage'),
     path('register/', views.register, name='register'),
     path('login/', views.custom_login, name='login'),
     path('works/', views.register, name='works'),
     path('schedule/', views.register, name='schedule'),
+    path('logout/', LogoutView.as_view(next_page='homepage'), name='logout'),
     ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def home(request):
     return render(request, "home.html", {})
 def register(request):
     return render(request, "home.html", {})
 
 def custom_login(request):
-    return render(request, "home.html", {})
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('homepage')  # Redirect all users to profile page
+
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+            return redirect('login')
+
+    return render(request, 'login.html', {'form': form})
