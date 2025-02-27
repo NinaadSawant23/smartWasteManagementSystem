e35ecb80f6583189b199661bdacab601211062e7
Ninaad Sawant <ninaadsawant23@gmail.com>
2025-02-26 19:54:21 -0500

    Alert messages added

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -7,8 +7,20 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     return render(request, "home.html", {})
+
 def register(request):
-    return render(request, "home.html", {})
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()  
+            login(request, user) 
+            messages.success(request, "Registration successful. Welcome!")
+            return redirect('homepage')  
+        else:
+            messages.error(request, "Registration failed. Please correct the errors below.")
+
+    return render(request, 'register.html', {'form': form})
 
 def custom_login(request):
     form = AuthenticationForm(request, data=request.POST or None)
@@ -21,6 +33,7 @@ def custom_login(request):
 
         if user:
             login(request, user)
+            messages.success(request, "Registration successful. Welcome!")
             return redirect('homepage')  # Redirect all users to profile page
 
         else:
