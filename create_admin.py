8f57f8f21e00b5e017a2a327751f50428e5cfd05
Gitesh Sagvekar <giteshvs307@gmail.com>
2025-04-30 07:59:25 -0400

    Create create_admin.py

---
+++ create_admin.py
@@ -0,0 +1,20 @@
+# create_admin.py
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')  # Replace with your settings module
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "team7")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "giteshvs07@gmail.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "team7")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
