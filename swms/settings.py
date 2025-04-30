02568d287d5559bac7e2bf9e3db56f619e0ee631
Gitesh Sagvekar <giteshvs307@gmail.com>
2025-04-30 19:40:19 -0400

    Update settings.py

--- swms/settings.py
+++ swms/settings.py
@@ -142,10 +142,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sagvekar799@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbvt yxxq ttma hfss'
-DEFAULT_FROM_EMAIL = 'noreply@wecanmass.org'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'sagvekar799@gmail.com'
+# EMAIL_HOST_PASSWORD = 'dbvt yxxq ttma hfss'
+# DEFAULT_FROM_EMAIL = 'noreply@wecanmass.org'
