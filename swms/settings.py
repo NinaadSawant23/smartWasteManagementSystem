ee59c11ff1a552acfaee2e887beb3083da681aed
Gitesh Sagvekar <giteshvs307@gmail.com>
2025-05-01 19:03:49 -0400

    Database updates

--- swms/settings.py
+++ swms/settings.py
@@ -140,7 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutvoField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
